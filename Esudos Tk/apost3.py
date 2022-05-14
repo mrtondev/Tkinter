d4c0b0edc696f0d7c6b7db496a35f27194499310
Ayrton Souza <ayrtonrafael@gmail.com>
2022-05-14 16:30:12 -0300

    aulaApostila

---
+++ Esudos Tk/apost3.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+
+class Janela:
+    def __init__(self,toplevel):
+        self.frl= Frame(toplevel)
+        self.frl.pack()
+
+        self.botao1 = Button(self.frl,text='Oi!')
+        self.botao1['background']='green'
+        self.botao1['font']=('Verdana','12','italic','bold')
+        self.botao1['height']=3
+        self.botao1.pack()
+
+        self.botao2 = Button(self.frl,bg='red', font=('Times','16'))
+        self.botao2['text']='Tchau!'
+        self.botao2['width']=12
+        self.botao2.pack()
+
+raiz=Tk()
+Janela(raiz)
+raiz.mainloop()
