0d21d86f31cbb89b22ab1373614e1a601c0a0047
Ayrton Rafael <ayrton.rafael@gmail.com>
2023-07-13 22:50:25 -0300

    cap 2

---
+++ Apostila/class_Tk.py
@@ -0,0 +1,8 @@
+from tkinter import *
+class Janela:
+    def __init__(self, instancia_de_Tk):
+        pass
+
+raiz=Tk()
+Janela(raiz)
+raiz.mainloop()
---
+++ Apostila/frame_Widget_Button.py
@@ -0,0 +1,12 @@
+from tkinter import *
+class Janela:
+    def __init__(self,toplevel):
+        self.frl = Frame(toplevel)
+        self.frl.pack()
+        self.botao = Button(self.frl, text= 'Oi!', background='green')
+        self.botao.pack()
+
+raiz=Tk()
+Janela(raiz)
+raiz.mainloop()
+        
